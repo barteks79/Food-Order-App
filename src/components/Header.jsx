789e2948944251a56781a609b49ebd871d6839da
bartosz.sarnowski <bartosz.sarnowski@o2.pl>
2024-07-26 22:42:35 +0200

    Added styles to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 export default function Header() {
 	return (
-		<header>
-			<div>
-				<img src="../src/assets/logo.jpg" alt="reactfood logo" />
-				<h1>reactfood</h1>
+		<header className="flex justify-between items-center py-24">
+			<div className="flex items-center gap-8">
+				<img className="border-4 border-white rounded-full h-28" src="../src/assets/logo.jpg" alt="reactfood logo" />
+				<h1 className="text-white uppercase font-semibold text-4xl tracking-wide">reactfood</h1>
 			</div>
-			<button>
-				<p>Cart()</p>
+			<button className="py-4 px-6 rounded hover:bg-yellow-700 transition-colors duration-150 ease-in-out">
+				<p className="text-2xl text-white font-medium">Cart (3)</p>
 			</button>
 		</header>
 	);
