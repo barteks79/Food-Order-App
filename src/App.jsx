9936b1b0c59d8c9acd12b69b95ed30a38e4fceac
bartosz.sarnowski <bartosz.sarnowski@o2.pl>
2024-07-26 23:03:56 +0200

    Added MealsMenu component and did some styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from './components/Header.jsx';
+import MealsMenu from './components/MealsMenu.jsx';
 
 function App() {
 	return (
 		<>
 			<Header />
+            <MealsMenu />
 		</>
 	);
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
 				<img className="border-4 border-white rounded-full h-28" src="../src/assets/logo.jpg" alt="reactfood logo" />
 				<h1 className="text-white uppercase font-semibold text-4xl tracking-wide">reactfood</h1>
 			</div>
-			<button className="py-4 px-6 rounded hover:bg-yellow-700 transition-colors duration-150 ease-in-out">
+			<button className="py-4 px-6 rounded hover:bg-yellow-600 transition-colors duration-150 ease-in-out">
 				<p className="text-2xl text-white font-medium">Cart (3)</p>
 			</button>
 		</header>
