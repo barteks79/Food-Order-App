56b4685d5e399e2de74e99afedf63e2d3ef72e51
bartosz.sarnowski <bartosz.sarnowski@o2.pl>
2024-07-28 13:46:45 +0200

    Created util.js file with price formatting function

--- src/components/Dish.jsx
+++ src/components/Dish.jsx
@@ -1,5 +1,6 @@
 import { useContext } from 'react';
 import { CartContext } from '../App';
+import { currencyFormatter } from '../util/util';
 import Button from './Button';
 
 export default function Dish({ mealData, name, price, description, image }) {
@@ -10,7 +11,9 @@ export default function Dish({ mealData, name, price, description, image }) {
 			<img className="rounded-ss-xl rounded-se-xl" src={`http://localhost:3000/${image}`} alt="Dish image" />
 			<div className="flex flex-col items-center pt-4 pb-8 gap-2 text-white">
 				<h2 className="text-2xl font-semibold pb-1">{name}</h2>
-				<p className="rounded-md text-primary font-medium px-10 py-2 bg-secondary text-lg">${price}</p>
+				<p className="rounded-md text-primary font-medium px-10 py-2 bg-secondary text-lg">
+					{currencyFormatter.format(price)}
+				</p>
 				<p className="pt-3 pb-5 px-6 text-center text-lg h-36">{description}</p>
 				<Button arg={mealData} onButtonClick={handleAddToCart}>
 					<p className="text-xl">Add to Cart</p>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
 		<header className="flex justify-between items-center py-24">
 			<div className="flex items-center gap-8">
 				<img className="border-4 border-primary rounded-full h-28" src="../src/assets/logo.jpg" alt="reactfood logo" />
-				<h1 className="text-primary uppercase font-semibold text-4xl tracking-wide">reactfood</h1>
+				<h1 className="text-primary uppercase font-semibold text-4xl tracking-wide">food order app</h1>
 			</div>
 			<Button onButtonClick={handleToggleModal}>
 				<p className="text-2xl">Cart ({userCart.length})</p>
